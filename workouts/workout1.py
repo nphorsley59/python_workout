ea7def7a397e6a957a17623c651125b3878857e8
nphorsley59 <nphorsley59@gmail.com>
2021-06-18 12:23:11 -0400

    added 'guess_prompt' function to workout1.py and created script for workout2.py

--- workouts/workout1.py
+++ workouts/workout1.py
@@ -9,11 +9,7 @@ def guessing_game():
     first_guess = True
   
     while True:
-        if first_guess:
-            guess_as_string = input("What number am I thinking of? ")
-            first_guess = False
-        else:   
-            guess_as_string = input("Try again: ")
+        guess_as_string = guess_prompt(first_guess)
         
         try:
             guess = int(guess_as_string)
@@ -30,5 +26,24 @@ def guessing_game():
             
         if answer < guess:
             print("Too high!")
+
+def guess_prompt(first_guess):
+    """This function determines which prompt to give the user, depending on 
+    whether or not it is their first guess.
+
+    Args:
+        first_guess ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """
+    
+    if first_guess:
+        guess_as_string = input("What number am I thinking of? ")
+        first_guess = False
+    else:   
+        guess_as_string = input("Try again: ")
+    
+    return guess_as_string;
 
 guessing_game()
